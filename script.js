ee453cdae9dbe533e85ee7f0047e6d44728c9f2b
Ahsan8381 <ahsan8381amu@gmail.com>
2025-08-10 01:14:31 +0530

    updated project

--- script.js
+++ script.js
@@ -22,3 +22,39 @@ setInterval(()=>{
     const idx=Math.floor(Math.random()*quotes.length);
     quotelement.textContent=quotes[idx];
 },4000);
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addTask = document.getElementById("addtask");
+
+addTask.addEventListener("click", function() {
+  if (inputBox.value === '') {
+    alert("Please write something to add.");
+  } else {
+    let li = document.createElement("li");
+    li.textContent = inputBox.value; 
+    listContainer.appendChild(li);
+    let span=document.createElement("span");
+    span.textContent="\u00d7";
+    li.appendChild(span);
+    inputBox.value = ''; 
+    saveData();
+  }
+});
+
+listContainer.addEventListener("click",function(e){
+  if(e.target.tagName==="LI"){
+    e.target.classList.toggle("checked");
+  }else if(e.target.tagName==="SPAN"){
+    e.target.parentElement.remove();
+  }
+  saveData();
+},false);
+
+function saveData(){
+  localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+  listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
